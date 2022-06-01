19cd55e4828bc6aa230a1f44a8e0037bc39a270e
Pedro Henrique Wegner <pedrowegner@gmail.com>
2022-06-01 18:12:20 -0300

    adicionado calculo em paralelo no algoritmo

--- k_means_paleta_bgr/Form1.cs
+++ k_means_paleta_bgr/Form1.cs
@@ -26,7 +26,7 @@ namespace k_means_paleta_bgr
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            bmp = new Bitmap(@"C:\facial_detection_project\k_means_paleta_bgr\k_means_paleta_bgr\norge_1.jpg");
+            bmp = new Bitmap(@"C:\facial_detection_project\k_means_paleta_bgr\k_means_paleta_bgr\china.jpg");
             g = Graphics.FromImage(bmp);
             picture.Image = bmp;
             picture.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -50,34 +50,34 @@ namespace k_means_paleta_bgr
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            Teste x = new Teste(bmp);
-
+            Teste imgFormated = new Teste(bmp);
+            // OrderBy(c => c[0] + c[1] + c[2]).ToList();
             KMeansBGR kmeans = new KMeansBGR();
-            List<int[]> colors = kmeans.clu(x.pixelsArray, 12);
-            palete1.BackColor = Color.FromArgb(colors[0][0], colors[0][1], colors[0][2]);
-            palete2.BackColor = Color.FromArgb(colors[1][0], colors[1][1], colors[1][2]);
-            palete3.BackColor = Color.FromArgb(colors[2][0], colors[2][1], colors[2][2]);
-            palete4.BackColor = Color.FromArgb(colors[3][0], colors[3][1], colors[3][2]);
-            palete5.BackColor = Color.FromArgb(colors[4][0], colors[4][1], colors[4][2]);
-            palete6.BackColor = Color.FromArgb(colors[5][0], colors[5][1], colors[5][2]);
-            palete7.BackColor = Color.FromArgb(colors[6][0], colors[6][1], colors[6][2]);
-            panete8.BackColor = Color.FromArgb(colors[7][0], colors[7][1], colors[7][2]);
-            palete9.BackColor = Color.FromArgb(colors[8][0], colors[8][1], colors[8][2]);
-            palete10.BackColor = Color.FromArgb(colors[9][0], colors[9][1], colors[9][2]);
-            palete11.BackColor = Color.FromArgb(colors[10][0], colors[10][1], colors[10][2]);
-            palete12.BackColor = Color.FromArgb(colors[11][0], colors[11][1], colors[11][2]);
-            cor1.Text = $"{ colors[0][0]}, {colors[0][1]}, {colors[0][2]}";
-            cor2.Text = $"{colors[1][0]}, {colors[1][1]}, {colors[1][2]}";
-            cor3.Text = $"{colors[2][0]}, {colors[2][1]}, {colors[2][2]}";
-            cor4.Text = $"{colors[3][0]}, {colors[3][1]}, {colors[3][2]}";
-            cor5.Text = $"{colors[4][0]}, {colors[4][1]}, {colors[4][2]}";
-            cor6.Text = $"{colors[5][0]}, {colors[5][1]}, {colors[5][2]}";
-            cor7.Text = $"{colors[6][0]}, {colors[6][1]}, {colors[6][2]}";
-            cor8.Text = $"{colors[7][0]}, {colors[7][1]}, {colors[7][2]}";
-            cor9.Text = $"{colors[8][0]}, {colors[8][1]}, {colors[8][2]}";
-            cor10.Text = $"{colors[9][0]}, {colors[9][1]}, {colors[9][2]}";
-            cor11.Text = $"{colors[10][0]}, {colors[10][1]}, {colors[10][2]}";
-            cor12.Text = $"{colors[11][0]}, {colors[11][1]}, {colors[11][2]}";
+            List<Color> colors = kmeans.Cluterize(imgFormated.byteArray, 12, imgFormated.imgHeight, imgFormated.imgStride).OrderBy(c => c.R + c.G + c.B).ToList(); ;
+            palete1.BackColor = Color.FromArgb(colors[0].R, colors[0].G, colors[0].B);
+            palete2.BackColor = Color.FromArgb(colors[1].R, colors[1].G, colors[1].B);
+            palete3.BackColor = Color.FromArgb(colors[2].R, colors[2].G, colors[2].B);
+            palete4.BackColor = Color.FromArgb(colors[3].R, colors[3].G, colors[3].B);
+            palete5.BackColor = Color.FromArgb(colors[4].R, colors[4].G, colors[4].B);
+            palete6.BackColor = Color.FromArgb(colors[5].R, colors[5].G, colors[5].B);
+            palete7.BackColor = Color.FromArgb(colors[6].R, colors[6].G, colors[6].B);
+            panete8.BackColor = Color.FromArgb(colors[7].R, colors[7].G, colors[7].B);
+            palete9.BackColor = Color.FromArgb(colors[8].R, colors[8].G, colors[8].B);
+            palete10.BackColor = Color.FromArgb(colors[9].R, colors[9].G, colors[9].B);
+            palete11.BackColor = Color.FromArgb(colors[10].R, colors[10].G, colors[10].B);
+            palete12.BackColor = Color.FromArgb(colors[11].R, colors[11].G, colors[11].B);
+            cor1.Text = $"{colors[0].R}, {colors[0].G}, {colors[0].B}";
+            cor2.Text = $"{colors[1].R}, {colors[1].G}, {colors[1].B}";
+            cor3.Text = $"{colors[2].R}, {colors[2].G}, {colors[2].B}";
+            cor4.Text = $"{colors[3].R}, {colors[3].G}, {colors[3].B}";
+            cor5.Text = $"{colors[4].R}, {colors[4].G}, {colors[4].B}";
+            cor6.Text = $"{colors[5].R}, {colors[5].G}, {colors[5].B}";
+            cor7.Text = $"{colors[6].R}, {colors[6].G}, {colors[6].B}";
+            cor8.Text = $"{colors[7].R}, {colors[7].G}, {colors[7].B}";
+            cor9.Text = $"{colors[8].R}, {colors[8].G}, {colors[8].B}";
+            cor10.Text = $"{colors[9].R}, {colors[9].G}, {colors[9].B}";
+            cor11.Text = $"{colors[10].R}, {colors[10].G}, {colors[10].B}";
+            cor12.Text = $"{colors[11].R}, {colors[11].G}, {colors[11].B}";
             Console.WriteLine();
         }
     }
